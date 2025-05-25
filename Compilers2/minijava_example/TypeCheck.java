f941cf95e1a06df2741216d8a8f911a1a2594d75
“sdi2200008” <sdi2200008@di.uoa.gr>
2025-05-25 11:48:15 +0300

    mainclass typechecking

--- Compilers2/minijava_example/TypeCheck.java
+++ Compilers2/minijava_example/TypeCheck.java
@@ -10,5 +10,19 @@ public class TypeCheck extends GJDepthFirst<String, Void>{
         this.symbolTable = symbolTable;
     }
 
+    @Override
+    public String visit(MainClass n, Void argu) throws Exception {
+        String classname = n.f1.accept(this, null);
+        currentClass = symbolTable.getClass(classname);
+        currentMethod = currentClass.getMethod("main");
+
+        n.f14.accept(this, argu);
+        n.f15.accept(this, argu);
+
+        currentMethod = null;
+        currentClass = null;
+        return null;
+    }
+
     
 }
