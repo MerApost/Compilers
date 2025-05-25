1ba0a8a39d8d3d457f5ca1f40150b3a57111499a
“sdi2200008” <sdi2200008@di.uoa.gr>
2025-05-25 11:51:32 +0300

    class declaration typechecking

--- Compilers2/minijava_example/TypeCheck.java
+++ Compilers2/minijava_example/TypeCheck.java
@@ -1,3 +1,6 @@
+import Compilers2.minijava_example.syntaxtree.ClassDeclaration;
+import Compilers2.minijava_example.syntaxtree.MainClass;
+import Compilers2.minijava_example.visitor.GJDepthFirst;
 import syntaxtree.*;
 import visitor.*;
 
@@ -20,6 +23,18 @@ public class TypeCheck extends GJDepthFirst<String, Void>{
         n.f15.accept(this, argu);
 
         currentMethod = null;
+        currentClass = null;
+        return null;
+    }
+
+    @Override
+    public String visit(ClassDeclaration n, Void argu) throws Exception {
+        String classname = n.f1.accept(this, null);
+        currentClass = symbolTable.getClass(classname);
+
+        n.f3.accept(this, argu);
+        n.f4.accept(this, argu);
+
         currentClass = null;
         return null;
     }
