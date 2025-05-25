53643c61cead3b54e9ae66c5affca6299ff18cb5
“sdi2200008” <sdi2200008@di.uoa.gr>
2025-05-25 11:42:41 +0300

    visitor2-TypeCheck

--- Compilers2/minijava_example/TypeCheck.java
+++ Compilers2/minijava_example/TypeCheck.java
@@ -1,3 +1,14 @@
-public class TypeCheck {
+import syntaxtree.*;
+import visitor.*;
+
+public class TypeCheck extends GJDepthFirst<String, Void>{
+    private SymbolTable symbolTable;
+    private SymbolTable.ClassSymbol currentClass = null;
+    private SymbolTable.MethodSymbol currentMethod = null;
+
+    public TypeCheck(SymbolTable symbolTable) {
+        this.symbolTable = symbolTable;
+    }
+
     
 }
